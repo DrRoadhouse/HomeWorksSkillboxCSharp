e331aac02ebb5247a1aacb86c107b4e34d90c8c0
Dr.Roadhouse <68916506+DrRoadhouse@users.noreply.github.com>
2021-12-01 17:28:28 +0300

    Homework 2 commit

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Иванов Петр Петрович";
+            int  age = 23;
+            string email = "PetrPetrovich@gmail.com";
+            int programmingPoints = 4;
+            int mathPoints = 5;
+            int phisicPoints = 4;
+
+            WriteLine($" ФИО: {fullName}\n Возраст: {age}\n Email: {email}\n " +
+                $"Прогрммирование: {programmingPoints}\n Математика: {mathPoints}\n Физика: {phisicPoints} ");
+           
+            ReadKey();
+            double midPoint = (programmingPoints + mathPoints + phisicPoints) / 3d;
+            WriteLine($"Средний балл = {midPoint}");
+        }
+    }
+}
